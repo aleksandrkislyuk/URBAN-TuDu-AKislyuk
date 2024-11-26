ac1447eee866e6838275bb286d04dafcefa094e7
Aleksandr Kislyuk <sr28@inbox.ru>
2024-11-27 01:27:19 +0400

    third commit

--- script.js
+++ script.js
@@ -13,13 +13,54 @@ function displayTasks() {
 function displayTask(task) {
   const taskNumber = tasks.indexOf(task);
   taskList.innerHTML += `
-  <div id="task">
-  <h4>${task.title}</h4>
-  <p>${task.status}</p>
-  <button id="button_delete" onclick="deleteTask(${taskNumber})">Удалить</button>
-  <button id="button_change" onclick="changeTask(${taskNumber})">Изменить</button> 
-  </div>
-  `; 
+    <div id="task" class="card">
+    <h4>${task.title}</h4>
+    <span>${task.status}</span>
+    <button id="button_delete" onclick="deleteTask(${taskNumber})">Удалить</button>
+    <button id="button_change" onclick="changeTask(${taskNumber})">Изменить</button> 
+    </div>
+    `;
+}
+
+function deleteTask(taskNumber) {
+  tasks.splice(taskNumber, 1);
+  displayTasks();
+  saveTasks();
+  filterTasks();
+  //reload();
+}
+
+function add_Task() {
+  const titleInput = document.getElementById('newTask');
+  if (!titleInput.value.trim()) {
+    alert("Поле ОТРЕДАКТИРУЙТЕ ЗАДАЧУ должно быть заполнено.");
+    return false;
+  }
+  const task = {};
+  task.title = document.getElementById('newTask').value;
+  task.status = document.getElementById('newStatus').value;
+  tasks.push(task);
+  
+  displayTasks();
+  saveTasks();
+  filterTasks();
+  modal.style.display = 'none'; // Скрыть модальное окно
+  backdrop.style.display = 'none'; // Скрыть фоновую подложку
+   //reload();
+  document.forms[1].reset(); //очистка формы
+  return false;
+}
+
+function changeTask(taskNumber) {
+  document.forms[1].reset(); //очистка формы модального окна
+  const selectedTaskList = tasks[taskNumber]; //определение идентификатора для поиска задачи в массиве
+  document.getElementById('newTask').value = selectedTaskList.title; //Заполнение формы
+  document.getElementById('newStatus').value = selectedTaskList.status; //Заполнение формы
+  const modal = document.getElementById('editModal');
+  const backdrop = document.getElementById('modalBackdrop');
+  modal.style.display = 'block'; // Показать модальное окно
+  backdrop.style.display = 'block'; // Показать фоновую подложку
+   deleteTask(taskNumber);    // Удаляем предыдущую карточку задачи
 }
 
 function loadTasks() {
@@ -30,53 +71,43 @@ function loadTasks() {
 
 function addTask() {
   const titleInput = document.getElementById('title');
-  const statusInput = document.getElementById('status');
+  //const statusInput = document.getElementById('status');
 
   if (!titleInput.value.trim()) {
     alert("Поле НОВАЯ ЗАДАЧА должно быть заполнено.");
     return false;
   }
 
-  const task = {
-    title: titleInput.value,
-    status: statusInput.value
-  };
+  const task = {};
+  task.title = document.getElementById('title').value;
+  task.status = document.getElementById('status').value;
 
   tasks.push(task);
   displayTasks();
   saveTasks();
   filterTasks();
+  reload();
   document.forms[0].reset(); //очистка формы
   return false;
 }
 
-function deleteTask(taskNumber) {
-  tasks.splice(taskNumber, 1);
-  displayTasks();
-  saveTasks();
+function deleteArray() {
+  localStorage.clear();
+  reload();
+  alert('Список задач удалён.');
 }
 
 function reload() {
-  window.location.reload()
-};
+  window.location.reload();
+}
 
 function saveTasks() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function changeTask(taskNumber) {
-  /*очистка формы*/
-  document.forms[0].reset();
-  /*определение идентификатора для поиска книги в массиве*/
-  const selectedTaskList = tasks[taskNumber];
-  /*Заполнение формы*/
-  document.getElementById('title').value = selectedTaskList.title;
-  document.getElementById('status').value = selectedTaskList.status;
-  /*удалениие предыдущего варианта карточки книги*/
-  deleteTask(taskNumber);
-}
 
-/*составление списка книг по шаблону*/
+
+/*составление списка задач по шаблону*/
 const taskListHTML = tasks.map(task => {
   return `<li>${task.title},  ${task.status}.</li>`;
 }).join('\n');
@@ -103,23 +134,8 @@ function notReadTask() {
 let notRead = notReadTask();
 document.getElementById('notRead').innerHTML = `${notRead}`;
 
-//окно выбора - удалять/не удалять весь массив
-function deleteArray() {
-  
-  // Запрашиваем подтверждение у пользователя
-  const isConfirmed = confirm('Вы уверены, что хотите очистить список задач?');
 
-  if (isConfirmed) {
-    // Очистка localStorage
-    localStorage.clear();
-    alert('Список задач удалён.');
-  } else {
-    alert('УРА, НЕ УДАЛЯТЬ! - нажмите ЗАКРЫТЬ!');
-  }
-  displayTasks();
-  saveTasks();
-}
-   
+
 function filterTasks() {
   const filterTasks = document.getElementById('filterTasks');
   const filterText = filterTasks.value.toLowerCase();
@@ -132,6 +148,6 @@ function filterTasks() {
     }
     else {
       task.style.display = 'none';
-    }; 
+    };
   });
 }
